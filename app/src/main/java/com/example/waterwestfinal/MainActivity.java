85af1867b9eae32cb145258c6c4026b67708f4e4
ezar58 <89005549+ezar58@users.noreply.github.com>
2022-07-31 19:59:35 +0300

    still  broken :/

--- app/src/main/java/com/example/waterwestfinal/MainActivity.java
+++ app/src/main/java/com/example/waterwestfinal/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.waterwestfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,14 +11,41 @@ import android.widget.TextView;
 
 import com.example.waterwestfinal.R;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     Button AlertBtn;
     TextView WaterUsageBtn;
+    private RecyclerView mRecycler;
     androidx.constraintlayout.widget.Guideline topwater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRecycler = (RecyclerView) findViewById(R.id.todayL);
+        new FirebaseDatabaseHelper().readDays(new FirebaseDatabaseHelper.DataStatus() {
+            @Override
+            public void DataIsLoaded(List<Day> days, List<String> keys) {
+                new RecyclerView_Config().setConfig(mRecycler, MainActivity.this, days, keys);
+
+            }
+
+            @Override
+            public void DataIsInserted() {
+
+            }
+
+            @Override
+            public void DataIsUpdated() {
+
+            }
+
+            @Override
+            public void DataIsDeleted() {
+
+            }
+        });
         topwater = findViewById(R.id.watertop);
         //  topwater.setGuidelinePercent((float) 0.3); // to raise water in the tank
         WaterUsageBtn = findViewById(R.id.WaterUsageBtn);
